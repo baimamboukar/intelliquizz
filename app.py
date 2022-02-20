020eb6e2d34b54dabd5abc484743be85d0b1227d
baimamboukar <baimamboukar@gmail.com>
2022-02-20 18:08:19 +0100

    deploying live to heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from flask import Flask
 from database.connexion import IntelliquizzDB
 from helpers.converters import Converters
@@ -16,15 +17,15 @@ def index():
     data = worksheet.get_all_records()
     return json.dumps(data)
 
-@app.route("/<subject>/<level>/<number>/", )
-def anglo_ordinary(subject,level, number):
-    intelliDB = IntelliquizzDB()
-    db = intelliDB.connect()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM quizz WHERE subject = ? AND level = ? LIMIT ?", (subject,level, number))
-    data = cursor.fetchall()
-    objects_list = Converters.convert_questions(data)
-    return {"results": objects_list}, 200
+# @app.route("/<subject>/<level>/<number>/", )
+# def anglo_ordinary(subject,level, number):
+#     intelliDB = IntelliquizzDB()
+#     db = intelliDB.connect()
+#     cursor = db.cursor()
+#     cursor.execute("SELECT * FROM quizz WHERE subject = ? AND level = ? LIMIT ?", (subject,level, number))
+#     data = cursor.fetchall()
+#     objects_list = Converters.convert_questions(data)
+#     return {"results": objects_list}, 200
 
 
 @app.route("/<subject>/<level>/<number>/" )
@@ -32,11 +33,11 @@ def universal(subject,level,number):
     google_credentials = gspread.service_account(filename='credentials.json')
     sheet = google_credentials.open_by_key("1L6NurushGZT7vDXeR2v4kMYxxahkdhLkhdmLLROmECY")
     worksheet = sheet.worksheet(subject)
-    questions = [{'labe': '1', 'question': '2', 'answer': '3', 'subject': '4', 'level': '5', 'topic': '6', 'difficulty': '7', 'hint': '8', 'explanation': '9'}]
+    questions = []
     data = worksheet.get_all_records()
     for question in data:
         if(question["subject"] == subject and question["level"] == level):
-            questions.append(json.dumps(question))
+            questions.append(question)
             if(len(questions) == number):
                 break
-    return {"results": json.dumps(questions)}, 200
+    return {"results": questions}, 200
