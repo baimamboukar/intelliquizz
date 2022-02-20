a86fa6434ea441776233176f3d330cf8a3658f8e
baimamboukar <baimamboukar@gmail.com>
2022-02-20 17:36:59 +0100

    testing with sheets

--- sheets.py
+++ sheets.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2.token import NUMBER
 import gspread
 import json
 
@@ -7,12 +8,12 @@ DIFFICULTY = "medium"
 TOPIC = "Computer system"
 google_credentials = gspread.service_account(filename='credentials.json')
 sheet = google_credentials.open_by_key("1L6NurushGZT7vDXeR2v4kMYxxahkdhLkhdmLLROmECY")
-worksheet = sheet.worksheet("ICT")
+worksheet = sheet.worksheet(SUBJECT)
 questions = []
 data = worksheet.get_all_records()
 for question in data:
-    if(question["subject"] == SUBJECT and question["level"] == DIFFICULTY and question["topic"] == TOPIC):
-        questions.append(json.dumps(question))
-        if(len(questions) == NUMBER_OF_QUESTIONS):
-            break
+        if(question["subject"] == SUBJECT and question["level"] == DIFFICULTY):
+            questions.append(json.dumps(question))
+            if(len(questions) == NUMBER_OF_QUESTIONS):
+                break
 print(questions)
